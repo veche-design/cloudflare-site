de7be55937ef48ee10d3df90552138dbf9200d9c
vechedesign-alt <veche.design@gmail.com>
2026-09-17 15:09:31 +0200

    Photos III stage database

--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -3,13 +3,25 @@
 	import type { Pathname } from '$app/types';
 	import barefootStoreImage from '$lib/assets/images/barefoot-store.jpg';
 	import circularEconomyMallImage from '$lib/assets/images/circular-economy-mall.jpg';
+	import digitalDetoxLoungeImage from '$lib/assets/images/digital-detox-lounge.jpg';
 	import dumplingsImage from '$lib/assets/images/dumplings.jpg';
 	import eisenbahnImage from '$lib/assets/images/eisenbahn.jpg';
+	import ethiopianFoodBrandImage from '$lib/assets/images/ethiopian-food-brand.jpg';
+	import functionalBeverageBarImage from '$lib/assets/images/functional-beverage-bar.jpg';
+	import gesundheitskioskImage from '$lib/assets/images/gesundheitskiosk.jpg';
 	import herbalPharmacyImage from '$lib/assets/images/herbal-pharmacy.jpg';
+	import kioskChainImage from '$lib/assets/images/kiosk-chain.jpg';
+	import koreanJjimjilbangImage from '$lib/assets/images/korean-jjimjilbang.jpg';
+	import lifeSkillsSchoolImage from '$lib/assets/images/life-skills-school.jpg';
+	import microbiomStoreImage from '$lib/assets/images/microbiom-store.jpg';
+	import mushroomFarmImage from '$lib/assets/images/mushroom-farm.jpg';
 	import muslimFashionHouseImage from '$lib/assets/images/muslim-fashion-house.jpg';
+	import muslimFemaleClubImage from '$lib/assets/images/muslim-female-club.jpg';
 	import ogImage from '$lib/assets/images/og-image.jpg';
 	import rabiaImage from '$lib/assets/images/rabia.jpg';
 	import romantasyBookStoreImage from '$lib/assets/images/romantasy-book-store.jpg';
+	import ukrainianFoodBrandImage from '$lib/assets/images/ukrainian-food-brand.jpg';
+	import villageStoreFranchiseImage from '$lib/assets/images/village-store-franchise.jpg';
 	import DatabaseEntryCard from '$lib/components/DatabaseEntryCard.svelte';
 	import { m } from '$lib/paraglide/messages.js';
 	import { localizeHref } from '$lib/paraglide/runtime';
@@ -52,56 +64,103 @@
 			image: dumplingsImage,
 			imageAlt: 'Dumpling restaurant chain'
 		},
-		{ title: 'Soup Restaurant Franchise', industryTags: [m.database_industry_gastro()] },
-		{ title: 'Village Store Franchise', industryTags: [m.database_industry_retail()] },
+		{
+			title: 'Village Store Franchise',
+			industryTags: [m.database_industry_retail()],
+			image: villageStoreFranchiseImage,
+			imageAlt: 'Village Store Franchise'
+		},
 		{
 			title: 'Muslim Fashion House',
 			industryTags: [m.database_industry_retail()],
 			image: muslimFashionHouseImage,
 			imageAlt: 'Muslim Fashion House'
 		},
-		{ title: 'Walla Supermarket Franchise', industryTags: [m.database_industry_retail()] },
 		{
 			title: 'Circular Economy Mall',
 			industryTags: [m.database_industry_crafts(), m.database_industry_retail()],
 			image: circularEconomyMallImage,
 			imageAlt: 'Circular Economy Mall'
 		},
-		{ title: 'Kiosk Chain', industryTags: [m.database_industry_retail()] },
-		{ title: 'Mushroom Farm', industryTags: [m.database_industry_food()] },
+		{
+			title: 'Kiosk Chain',
+			industryTags: [m.database_industry_retail()],
+			image: kioskChainImage,
+			imageAlt: 'Kiosk Chain'
+		},
+		{
+			title: 'Mushroom Farm',
+			industryTags: [m.database_industry_food()],
+			image: mushroomFarmImage,
+			imageAlt: 'Mushroom Farm'
+		},
 		{
 			title: 'Model Eisenbahn Store and Maker Club',
 			industryTags: [m.database_industry_retail()],
 			image: eisenbahnImage,
 			imageAlt: 'Model Eisenbahn Store and Maker Club'
 		},
-		{ title: 'Ethiopian Food Brand', industryTags: [m.database_industry_food()] },
+		{
+			title: 'Ethiopian Food Brand',
+			industryTags: [m.database_industry_food()],
+			image: ethiopianFoodBrandImage,
+			imageAlt: 'Ethiopian Food Brand'
+		},
 		{
 			title: 'Ukrainian Food Brand',
-			industryTags: [m.database_industry_food(), m.database_industry_gastro()]
+			industryTags: [m.database_industry_food(), m.database_industry_gastro()],
+			image: ukrainianFoodBrandImage,
+			imageAlt: 'Ukrainian Food Brand'
 		},
-		{ title: 'Central Ticket Shop for Frankfurt', industryTags: [m.database_industry_arts()] },
 		{
 			title: 'Romantasy Book Store & Bar',
 			industryTags: [m.database_industry_retail(), m.database_industry_gastro()],
 			image: romantasyBookStoreImage,
 			imageAlt: 'Romantasy Book Store & Bar'
 		},
-		{ title: 'Muslim Female Club', industryTags: [m.database_industry_community()] },
+		{
+			title: 'Muslim Female Club',
+			industryTags: [m.database_industry_community()],
+			image: muslimFemaleClubImage,
+			imageAlt: 'Muslim Female Club'
+		},
 		{
 			title: 'Microbiom Store',
-			industryTags: [m.database_industry_health(), m.database_industry_retail()]
+			industryTags: [m.database_industry_health(), m.database_industry_retail()],
+			image: microbiomStoreImage,
+			imageAlt: 'Microbiom Store'
 		},
-		{ title: 'Gesundheitskiosk', industryTags: [m.database_industry_health()] },
-		{ title: 'Wearable & CGM Fitting Studio', industryTags: [m.database_industry_health()] },
+		{
+			title: 'Gesundheitskiosk',
+			industryTags: [m.database_industry_health()],
+			image: gesundheitskioskImage,
+			imageAlt: 'Gesundheitskiosk'
+		},
 		{
 			title: 'Functional Beverage Bar',
-			industryTags: [m.database_industry_gastro(), m.database_industry_health()]
+			industryTags: [m.database_industry_gastro(), m.database_industry_health()],
+			image: functionalBeverageBarImage,
+			imageAlt: 'Functional Beverage Bar'
 		},
-		{ title: 'Korean Jjimjilbang', industryTags: [m.database_industry_health()] },
-		{ title: 'Telemedicine Cabin', industryTags: [m.database_industry_health()] },
-		{ title: 'Digital Detox / Focus Lounge', industryTags: [m.database_industry_health()] },
-		{ title: 'Life Skills School for Kids', industryTags: [m.database_industry_community()] }
+		{
+			title: 'Korean Jjimjilbang',
+			industryTags: [m.database_industry_health()],
+			image: koreanJjimjilbangImage,
+			imageAlt: 'Korean Jjimjilbang'
+		},
+		{
+			title: 'Digital Detox / Focus Lounge',
+			industryTags: [m.database_industry_health()],
+			image: digitalDetoxLoungeImage,
+			imageAlt: 'Digital Detox / Focus Lounge'
+		},
+		{
+			title: 'Life Skills School for Kids',
+			industryTags: [m.database_industry_community()],
+			image: lifeSkillsSchoolImage,
+			imageAlt: 'Life Skills School for Kids'
+		},
+		{ title: 'Walla Supermarket Franchise', industryTags: [m.database_industry_retail()] }
 	];
 
 	const homeHref = resolve(localizeHref('/') as Pathname);
